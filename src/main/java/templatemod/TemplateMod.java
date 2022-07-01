b3d0ceac4b68d917cfddce1050402e6ae16a712b
Lyrthras <defelixanne@gmail.com>
2022-07-01 20:06:12 +0800

    First commit.

---
+++ src/main/java/templatemod/TemplateMod.java
@@ -0,0 +1,26 @@
+package templatemod;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class TemplateMod {
+    public void preInit() {
+
+    }
+
+    public void init() {
+        System.out.println("Hello from Template Mod.");
+    }
+
+    public void initResources() {
+
+    }
+
+    public void postInit() {
+
+    }
+
+    public void dispose() {
+
+    }
+}
